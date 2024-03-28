0fe6bd2230b0357ec11d2dce60fa69141266a6c6
Ycanzzay <steam3bera228@gmail.com>
2024-03-28 20:32:23 +0300

    edit classes

--- src/by/itstep/aniskovich/java/lesson36/controller/Box.java
+++ src/by/itstep/aniskovich/java/lesson36/controller/Box.java
@@ -1,20 +1,20 @@
 package by.itstep.aniskovich.java.lesson36.controller;
 
-public class Box {
-    private int value;
+public class Box<T> {
+    private T value;
 
     public Box() {
     }
 
-    public Box(int value) {
+    public Box(T value) {
         this.value = value;
     }
 
-    public int getValue() {
+    public T getValue() {
         return value;
     }
 
-    public void setValue(int value) {
+    public void setValue(T value) {
         this.value = value;
     }
 
@@ -22,4 +22,5 @@ public class Box {
     public String toString() {
         return value + "";
     }
+
 }
--- src/by/itstep/aniskovich/java/lesson36/controller/Main.java
+++ src/by/itstep/aniskovich/java/lesson36/controller/Main.java
@@ -1,13 +1,39 @@
 package by.itstep.aniskovich.java.lesson36.controller;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        ObjectBox box = new ObjectBox();
-        box.setValue(15);
-        box.setValue("abc");
-        box.setValue(new A());
-        //...
-        A a = (A) box.getValue();
+        Box<Arrays> sBox = new Box<>(); //before JDK 7.0
+
+
+
+
+
+
+//        ObjectBox box = new ObjectBox();
+//        box.setValue(15);
+//        box.setValue("abc");
+//        box.setValue(new A());
+//        //..
+//        box.setValue(10);
+//        //...
+////        A a = (A) box.getValue();
+//
+//        Object o = box.getValue();
+//        if(o instanceof String) {
+//            String s = (String) o;
+//            System.out.println(s + s);
+//        } else if (o instanceof Integer) {
+//            int n = (int) o;
+//            System.out.println(n * n);
+//        } else if (o instanceof A) {
+//            A a = (A) o;
+//            System.out.println(a);
+//        }
+//
+
+//        System.out.println(a.getClass().getName());
 
 //        Box box1= new Box();
 //        box1.setValue(10);
